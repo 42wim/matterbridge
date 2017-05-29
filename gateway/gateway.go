c791423dd5c56d6fbf5908e7bf8b7343b908337d
Wim <wim@42.be>
2017-05-30 00:11:53 +0200

    Add NOPINGNICK option. Closes #175

--- gateway/gateway.go
+++ gateway/gateway.go
@@ -234,6 +234,7 @@ func (gw *Gateway) modifyUsername(msg *config.Message, dest *bridge.Bridge) {
 	if nick == "" {
 		nick = dest.Config.RemoteNickFormat
 	}
+	nick = strings.Replace(nick, "{NOPINGNICK}", msg.Username[:1]+"​"+msg.Username[1:], -1)
 	nick = strings.Replace(nick, "{NICK}", msg.Username, -1)
 	nick = strings.Replace(nick, "{BRIDGE}", br.Name, -1)
 	nick = strings.Replace(nick, "{PROTOCOL}", br.Protocol, -1)
