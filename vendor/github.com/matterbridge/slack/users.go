b2a6777995c55233a47212743c781a27dde8dce6
Wim <wim@42.be>
2017-11-02 17:09:34 +0100

    Use matterbridge vendored slack

--- vendor/github.com/matterbridge/slack/users.go
+++ vendor/github.com/matterbridge/slack/users.go
@@ -15,24 +15,26 @@ const (
 
 // UserProfile contains all the information details of a given user
 type UserProfile struct {
-	FirstName          string `json:"first_name"`
-	LastName           string `json:"last_name"`
-	RealName           string `json:"real_name"`
-	RealNameNormalized string `json:"real_name_normalized"`
-	Email              string `json:"email"`
-	Skype              string `json:"skype"`
-	Phone              string `json:"phone"`
-	Image24            string `json:"image_24"`
-	Image32            string `json:"image_32"`
-	Image48            string `json:"image_48"`
-	Image72            string `json:"image_72"`
-	Image192           string `json:"image_192"`
-	ImageOriginal      string `json:"image_original"`
-	Title              string `json:"title"`
-	BotID              string `json:"bot_id,omitempty"`
-	ApiAppID           string `json:"api_app_id,omitempty"`
-	StatusText         string `json:"status_text,omitempty"`
-	StatusEmoji        string `json:"status_emoji,omitempty"`
+  FirstName             string `json:"first_name"`
+  LastName              string `json:"last_name"`
+  RealName              string `json:"real_name"`
+  RealNameNormalized    string `json:"real_name_normalized"`
+  DisplayName           string `json:"display_name"`
+  DisplayNameNormalized string `json:"display_name_normalized"`
+  Email                 string `json:"email"`
+  Skype                 string `json:"skype"`
+  Phone                 string `json:"phone"`
+  Image24               string `json:"image_24"`
+  Image32               string `json:"image_32"`
+  Image48               string `json:"image_48"`
+  Image72               string `json:"image_72"`
+  Image192              string `json:"image_192"`
+  ImageOriginal         string `json:"image_original"`
+  Title                 string `json:"title"`
+  BotID                 string `json:"bot_id,omitempty"`
+  ApiAppID              string `json:"api_app_id,omitempty"`
+  StatusText            string `json:"status_text,omitempty"`
+  StatusEmoji           string `json:"status_emoji,omitempty"`
 }
 
 // User contains all the information of a user
