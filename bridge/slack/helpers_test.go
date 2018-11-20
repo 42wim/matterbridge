adb02771c6d3f63d2664859ad553f74b0ded7c5c
Patrick Connolly <patrick.c.connolly@gmail.com>
2018-11-21 04:38:35 +0800

    Made gofmt happy.

--- bridge/slack/helpers_test.go
+++ bridge/slack/helpers_test.go
@@ -7,22 +7,22 @@ import (
 )
 
 func TestExtractTopicOrPurpose(t *testing.T) {
-	testcases := []struct{
-		input string
+	testcases := []struct {
+		input          string
 		wantChangeType string
-		wantOutput string
+		wantOutput     string
 	}{
-		{"@someone set channel topic: one liner",    "topic",   "one liner"},
-		{"@someone set channel purpose: one liner",  "purpose", "one liner"},
-		{"@someone set channel topic: multi\nliner", "topic",   "multi\nliner"},
-		{"@someone cleared channel topic",           "topic",   ""},
-		{"some unmatched message",                   "unknown", ""},
+		{"@someone set channel topic: one liner", "topic", "one liner"},
+		{"@someone set channel purpose: one liner", "purpose", "one liner"},
+		{"@someone set channel topic: multi\nliner", "topic", "multi\nliner"},
+		{"@someone cleared channel topic", "topic", ""},
+		{"some unmatched message", "unknown", ""},
 	}
 
 	for _, tc := range testcases {
 		gotChangeType, gotOutput := extractTopicOrPurpose(tc.input)
 
-		assert.Equal(t, tc.wantChangeType , gotChangeType)
-		assert.Equal(t, tc.wantOutput , gotOutput)
+		assert.Equal(t, tc.wantChangeType, gotChangeType)
+		assert.Equal(t, tc.wantOutput, gotOutput)
 	}
 }
