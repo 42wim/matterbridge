8548b69e6ec3b36521c6881a5d2bcb133012b6d9
Wim <wim@42.be>
2019-01-24 21:51:52 +0100

    Fix possible data race (irc). Closes #693

--- bridge/irc/irc.go
+++ bridge/irc/irc.go
@@ -85,10 +85,10 @@ func (b *Birc) Connect() error {
 	i.Handlers.Add(girc.RPL_WELCOME, b.handleNewConnection)
 	i.Handlers.Add(girc.RPL_ENDOFMOTD, b.handleOtherAuth)
 	i.Handlers.Add(girc.ALL_EVENTS, b.handleOther)
+	b.i = i
 
 	go b.doConnect()
 
-	b.i = i
 	err = <-b.connected
 	if err != nil {
 		return fmt.Errorf("connection failed %s", err)
