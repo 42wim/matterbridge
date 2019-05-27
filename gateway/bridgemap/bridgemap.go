3fb3422048be7bbcac5f4b1fbb96f86e8a6871e1
Duco van Amstel <helcaraxan@gmail.com>
2019-05-27 21:17:31 +0100

    Only forward user-typing messages if supported by protocol
    Fixes issue #814.
    
    This is a somewhat hacky way of achieving the required goal but it seems
    like this is the least problematic way of getting there.
    
    We might want to redesign some bridge information later such that we
    have a standardised way of specifying what is and what isn't supported
    by each chat protocol / bridge.

--- gateway/bridgemap/bridgemap.go
+++ gateway/bridgemap/bridgemap.go
@@ -3,35 +3,41 @@ package bridgemap
 import (
 	"github.com/42wim/matterbridge/bridge"
 	"github.com/42wim/matterbridge/bridge/api"
-	"github.com/42wim/matterbridge/bridge/discord"
-	"github.com/42wim/matterbridge/bridge/gitter"
-	"github.com/42wim/matterbridge/bridge/irc"
-	"github.com/42wim/matterbridge/bridge/matrix"
-	"github.com/42wim/matterbridge/bridge/mattermost"
-	"github.com/42wim/matterbridge/bridge/rocketchat"
-	"github.com/42wim/matterbridge/bridge/slack"
-	"github.com/42wim/matterbridge/bridge/sshchat"
-	"github.com/42wim/matterbridge/bridge/steam"
-	"github.com/42wim/matterbridge/bridge/telegram"
-	"github.com/42wim/matterbridge/bridge/whatsapp"
-	"github.com/42wim/matterbridge/bridge/xmpp"
-	"github.com/42wim/matterbridge/bridge/zulip"
+	bdiscord "github.com/42wim/matterbridge/bridge/discord"
+	bgitter "github.com/42wim/matterbridge/bridge/gitter"
+	birc "github.com/42wim/matterbridge/bridge/irc"
+	bmatrix "github.com/42wim/matterbridge/bridge/matrix"
+	bmattermost "github.com/42wim/matterbridge/bridge/mattermost"
+	brocketchat "github.com/42wim/matterbridge/bridge/rocketchat"
+	bslack "github.com/42wim/matterbridge/bridge/slack"
+	bsshchat "github.com/42wim/matterbridge/bridge/sshchat"
+	bsteam "github.com/42wim/matterbridge/bridge/steam"
+	btelegram "github.com/42wim/matterbridge/bridge/telegram"
+	bwhatsapp "github.com/42wim/matterbridge/bridge/whatsapp"
+	bxmpp "github.com/42wim/matterbridge/bridge/xmpp"
+	bzulip "github.com/42wim/matterbridge/bridge/zulip"
 )
 
-var FullMap = map[string]bridge.Factory{
-	"api":          api.New,
-	"discord":      bdiscord.New,
-	"gitter":       bgitter.New,
-	"irc":          birc.New,
-	"mattermost":   bmattermost.New,
-	"matrix":       bmatrix.New,
-	"rocketchat":   brocketchat.New,
-	"slack-legacy": bslack.NewLegacy,
-	"slack":        bslack.New,
-	"sshchat":      bsshchat.New,
-	"steam":        bsteam.New,
-	"telegram":     btelegram.New,
-	"whatsapp":     bwhatsapp.New,
-	"xmpp":         bxmpp.New,
-	"zulip":        bzulip.New,
-}
+var (
+	FullMap = map[string]bridge.Factory{
+		"api":          api.New,
+		"discord":      bdiscord.New,
+		"gitter":       bgitter.New,
+		"irc":          birc.New,
+		"mattermost":   bmattermost.New,
+		"matrix":       bmatrix.New,
+		"rocketchat":   brocketchat.New,
+		"slack-legacy": bslack.NewLegacy,
+		"slack":        bslack.New,
+		"sshchat":      bsshchat.New,
+		"steam":        bsteam.New,
+		"telegram":     btelegram.New,
+		"whatsapp":     bwhatsapp.New,
+		"xmpp":         bxmpp.New,
+		"zulip":        bzulip.New,
+	}
+
+	UserTypingSupport = map[string]struct{}{
+		"slack": {},
+	}
+)
