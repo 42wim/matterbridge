176581a8fc9be213a24d25e890b4f0cdb53c7be3
Wim <wim@42.be>
2020-02-15 17:49:21 +0100

    Make avatars download work with mediaserverdownload (telegram). Fixes #920

--- bridge/telegram/handlers.go
+++ bridge/telegram/handlers.go
@@ -95,7 +95,7 @@ func (b *Btelegram) handleUsername(rmsg *config.Message, message *tgbotapi.Messa
 			}
 		}
 		// only download avatars if we have a place to upload them (configured mediaserver)
-		if b.General.MediaServerUpload != "" {
+		if b.General.MediaServerUpload != "" || (b.General.MediaServerDownload != "" && b.General.MediaDownloadPath != "") {
 			b.handleDownloadAvatar(message.From.ID, rmsg.Channel)
 		}
 	}
