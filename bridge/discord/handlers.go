98033b1ba77fcf55378eccf35c2a251b1278464b
Qais Patankar <qaisjp@gmail.com>
2020-03-22 17:39:11 +0000

    Don't transmit typing events from ourselves (slack/discord) (#1056)

--- bridge/discord/handlers.go
+++ bridge/discord/handlers.go
@@ -36,6 +36,11 @@ func (b *Bdiscord) messageTyping(s *discordgo.Session, m *discordgo.TypingStart)
 		return
 	}
 
+	// Ignore our own typing messages
+	if m.UserID == b.userID {
+		return
+	}
+
 	rmsg := config.Message{Account: b.Account, Event: config.EventUserTyping}
 	rmsg.Channel = b.getChannelName(m.ChannelID)
 	b.Remote <- rmsg
