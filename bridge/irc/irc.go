5847f7758ce0e9fbad53766efe495d7ba15adae4
TheHolyRoger <39387497+TheHolyRoger@users.noreply.github.com>
2020-06-24 22:48:54 +0100

    Only colour IRC nicks if there is one. (#1161)

--- bridge/irc/irc.go
+++ bridge/irc/irc.go
@@ -206,7 +206,7 @@ func (b *Birc) doSend() {
 	for msg := range b.Local {
 		<-throttle.C
 		username := msg.Username
-		if b.GetBool("Colornicks") {
+		if b.GetBool("Colornicks") && len(username) > 1 {
 			checksum := crc32.ChecksumIEEE([]byte(msg.Username))
 			colorCode := checksum%14 + 2 // quick fix - prevent white or black color codes
 			username = fmt.Sprintf("\x03%02d%s\x0F", colorCode, msg.Username)
